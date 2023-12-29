90023ecc05276b44b3a69b501db63a900d949537
Zed0825 <www.ragaszedrick25@gmail.com>
2023-12-29 17:01:06 +0800

    Updated avbar

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,19 @@
 function mobileMenu(){
     const menuButton = document.getElementById('menu');
-    
+    const menuContainer = document.getElementById('menu-container');
+    const closeButton = document.getElementById('close-button');
+    const cover = document.getElementById('cover');
+
+    menuButton.onclick = function () {
+        menuContainer.classList.toggle('active');
+        cover.classList.toggle('active');
+    };
+
+    closeButton.onclick = function () {
+        menuContainer.classList.remove('active');
+        cover.classList.remove('active');
+    };
 };
+
+window.onload = 
+mobileMenu();
