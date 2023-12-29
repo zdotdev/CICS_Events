2d357a3070c156f683c6021740eddc5875a4fd4e
Zed0825 <www.ragaszedrick25@gmail.com>
2023-12-30 00:06:51 +0800

    responsive navbar

--- javascript/index.js
+++ javascript/index.js
@@ -3,15 +3,18 @@ function mobileMenu(){
     const menuContainer = document.getElementById('menu-container');
     const closeButton = document.getElementById('close-button');
     const cover = document.getElementById('cover');
+    const body = document.querySelector('.body');
 
     menuButton.onclick = function () {
         menuContainer.classList.toggle('active');
         cover.classList.toggle('active');
+        body.classList.toggle('scroll-off');
     };
 
     closeButton.onclick = function () {
         menuContainer.classList.remove('active');
         cover.classList.remove('active');
+        body.classList.remove('scroll-off');
     };
 };
 
